bdc50cd187471cfc71111d154d2e5ec41b318097
Noor <noormahammad@users.noreply.github.com>
2020-01-24 18:39:18 -0500

    Main buttn text align

--- components/MainButton.js
+++ components/MainButton.js
@@ -21,6 +21,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20, 
     backgroundColor: Colors.primary,
     borderRadius: 20,
+    alignItems:'center'
   },
   buttonText: {
     fontFamily: 'OpenSans-Bold', 
