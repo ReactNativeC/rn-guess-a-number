751b20b50858eab49c5e57d860b3848c444a0efb
Noor <noormahammad@users.noreply.github.com>
2020-01-26 17:26:08 -0500

    Platform API to detect if running on Android or iOS

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {View, Text, StyleSheet, Dimensions, Platform} from 'react-native';
 import Colors from '../constants/colors';
 
 
@@ -15,16 +15,18 @@ const styles =  StyleSheet.create({
   header: {
     width: '100%',
     height: 90,
-    backgroundColor: Colors.primary,
+    backgroundColor: Platform.OS == 'android' ? Colors.primary : 'white',
     paddingTop: 36,
     justifyContent: 'center',
     alignItems: 'center',     
+    borderBottomColor: Platform.OS == 'ios' ? '#ccc' : 'white',
+    borderBottomWidth: Platform.OS == 'ios' ? 1 : 0,
   },
   headerTitle: {
     fontSize: Dimensions.get('window').width < 400 ? 18 : 25, 
     textAlign: 'center',
     fontFamily: 'OpenSans-Bold',
-    color: 'white',
+    color: Platform.OS == 'ios' ? Colors.primary : 'white',
   },
 });
 
