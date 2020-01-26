c2827f1f5e79131f72c37ac6ce92942c031fc642
Noor <noormahammad@users.noreply.github.com>
2020-01-26 18:21:04 -0500

    adding SafeAreaView  to App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 import Header from "./components/Header";
 import StartGameScreen from './screens/StartGameScreen';
@@ -57,10 +57,10 @@ export default function App() {
   //test
   //content = <GameOverScreen />
   return (    
-      <View style={styles.screen}>      
+      <SafeAreaView style={styles.screen}>      
         <Header title="Guess a Number" />
         {content}      
-      </View>    
+      </SafeAreaView>    
   );
 }
 
